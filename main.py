06d6b7148eb69c3d74941f697f81ad0b2d2e3ab9
abhijeet-b <56281551+abhijeet-b@users.noreply.github.com>
2021-06-10 14:53:36 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,61 @@
+import numpy as np
+import sympy as sp
+import pandas as pd
+np.set_printoptions(threshold=np.inf)
+
+# A = 3N * 3N matrix
+# N = total layers
+# serieslist = list of values in series
+# S = series layers
+# P = parallel layers
+# x = iteration variable 
+
+x=1
+N = int(input('Enter the amount of TOTAL LAYERS your transformer has: '))
+if(N==0):
+    quit()
+A=np.zeros((int(3*N),int(3*N)))
+serieslist = []
+
+while 1:
+    x = int(input('Enter, one by one, a series of layer #\'s that are in series: (sentinel is 0)'))
+    if(x==0): break
+    if x in serieslist:
+        print("Already entered this layer #, please enter another layer #")
+    elif((x<0) or (x>N)):
+        print("Out of bounds with range, layer not counted; please enter another value")
+    else:
+        serieslist.append(x)
+print(serieslist)
+serieslist.sort()
+
+S = len(serieslist)
+P = N - S
+# series identities == all equal to I_p
+j = 0
+for x in serieslist:
+    A[j][x] = 1
+    j=j+1
+    if j > S: 
+        break
+
+
+for x in range(1,N):
+     A[j][x-1] = -1
+     f = j
+     A[j][int((x*3)-2)]=2
+     A[j][int((x*3)-1)]=2
+     j=j+1
+
+    
+# test at the end 
+#print(A)
+
+df = pd.DataFrame (A)
+
+## save to xlsx file
+
+filepath = 'table.xlsx'
+
+df.to_excel(filepath, index=False)
+
