09e90b4501d0e8b4b925b571ebd684430ee22730
Michael Solomentsev <michaelsolomentsev@wireless-10-145-3-80.public.utexas.edu>
2021-07-21 14:04:35 -0500

    H field numbering

--- main.py
+++ main.py
@@ -10,24 +10,25 @@ np.set_printoptions(threshold=np.inf)
 # S = series layers
 # P = parallel layers
 # x = iteration variable 
-b,d,f=sp.symbols("b,d,f")
+b,d,f,l,r=sp.symbols("b,d,f,l,r")
+#create array of R symbolic values?
 x=1
 N = int(input('Enter the amount of TOTAL LAYERS your transformer has: '))
 if(N==0):
-    quit()
+	quit()
 A=np.zeros((int(3*N),int(3*N)))
 SM=sp.Matrix(A)
 serieslist = []
 
 while 1:
-    x = int(input('Enter, one by one, a series of layer #\'s that are in series: (sentinel is 0)'))
-    if(x==0): break
-    if x in serieslist:
-        print("Already entered this layer #, please enter another layer #")
-    elif((x<0) or (x>N)):
-        print("Out of bounds with range, layer not counted; please enter another value")
-    else:
-        serieslist.append(x)
+	x = int(input('Enter, one by one, a series of layer #\'s that are in series: (sentinel is 0)'))
+	if(x==0): break
+	if x in serieslist:
+		print("Already entered this layer #, please enter another layer #")
+	elif((x<0) or (x>N)):
+		print("Out of bounds with range, layer not counted; please enter another value")
+	else:
+		serieslist.append(x)
 #print(serieslist)
 serieslist.sort()
 
@@ -40,42 +41,49 @@ for i in serieslist:
 	if i in parallelList:
 		parallelList.remove(i)
 
-#print(parallelList)
+print(parallelList)
 S = len(serieslist)
 P = N - S
 # series identities == all equal to I_p
 j = 0
 for x in serieslist:
-    SM[j,x] = 1
-    j=j+1
-    if j > S: 
-        break
+	SM[j,x-1] = 1
+	j=j+1
+	if j > S: 
+		break
 
 
 for x in range(1,N+1):
-     z = N
-     SM[j,x-1] = -1
-     SM[j,z+2*(x-1)]=b # A[j][z+2*x-2]
-     SM[j,z+2*(x-1)+1]=b #A[j][z+2*x-1]
-     j=j+1
+	 z = N
+	 SM[j,x-1] = -1
+	 SM[j,z+2*(x-1)]=b # A[j][z+2*x-2]
+	 SM[j,z+2*(x-1)+1]=b #A[j][z+2*x-1]
+	 j=j+1
 
 
 SM[j,N] = 1
 j=j+1
 
 for x in range(0,N-1):
-    SM[j,N+2*x+1]=1
-    SM[j,N+2*x+2]=1
-    j=j+1
+	SM[j,N+2*x+1]=1
+	SM[j,N+2*x+2]=1
+	j=j+1
 SM[j,3*N-1]=1
 j=j+1
 
 for x in range(0,P-1):
-    for k in range(0,parallelList[x]):
-        SM[j,k] = k + 1
-    SM[j,N + 2*parallelList[x] - 1] = d/2
-    SM[j,N + 2*parallelList[x+1] - 2] = -d/2
-    j=j+1
+	r_range = parallelList[x+1] - parallelList[x]
+
+	for k in range(0,parallelList[x+1]-1):
+		if (k+1) > parallelList[x]:
+			r_count = r_range - k -1 + parallelList[x]
+		else:
+			r_count = r_range
+		SM[j,k] = r_count*r*l/b
+		
+	SM[j,N + 2*parallelList[x] - 1] = d/2
+	SM[j,N + 2*parallelList[x+1] - 2] = -d/2
+	j=j+1
 
 var_b = float(input('Layer width in cm:'))/100; 
 var_f = float(input('Operating frequency in MHz:'))*1000000
