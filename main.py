a470d9995b916fbb22df0c2a43c1bde32004160c
abhijeet-b <56281551+abhijeet-b@users.noreply.github.com>
2021-07-21 20:13:05 -0500

    fully symbolic matrices (except solving)

--- main.py
+++ main.py
@@ -11,14 +11,32 @@ np.set_printoptions(threshold=np.inf)
 # P = parallel layers
 # x = iteration variable 
 b,d,f,l,r=sp.symbols("b,d,f,l,r")
+
 #create array of R symbolic values?
 x=1
 N = int(input('Enter the amount of TOTAL LAYERS your transformer has: '))
 if(N==0):
 	quit()
-A=np.zeros((int(3*N),int(3*N)))
-SM=sp.Matrix(A)
+SM=sp.zeros(int(3*N),int(3*N))
 serieslist = []
+
+B=sp.zeros(int(3*N),int(1))
+I = sp.symbols('I1:%d'%(N+1))
+Kt = sp.symbols('Kt1:%d'%(N+1))
+Kb = sp.symbols('Kb1:%d'%(N+1))
+for x in range(0,N):
+    B[x,0] = I[x]
+    x+=2
+s = 0
+z=x
+x-=1
+for t in Kt:
+    B[x,0] = t
+    x+=2
+x=z
+for t in Kb:
+    B[x,0] = t
+    x+=2
 
 while 1:
 	x = int(input('Enter, one by one, a series of layer #\'s that are in series: (sentinel is 0)'))
@@ -41,7 +59,7 @@ for i in serieslist:
 	if i in parallelList:
 		parallelList.remove(i)
 
-print(parallelList)
+#print(parallelList)
 S = len(serieslist)
 P = N - S
 # series identities == all equal to I_p
@@ -89,9 +107,17 @@ var_b = float(input('Layer width in cm:'))/100;
 var_f = float(input('Operating frequency in MHz:'))*1000000
 var_d = 2/((2*math.pi*f)*(4*math.pi*math.pow(10,-7))*(1.68*math.pow(10,-8)))
 
+Ip = sp.symbols('Ip')
+z = 0
+C = sp.zeros(int(3*N),1)
+for x in parallelList:
+    C[z,0] = Ip
+    z+=1
+sp.pprint(SM)
+sp.pprint(B)
+
 SM.subs(b, var_b)
 SM.subs(d, var_d)
-sp.pprint(SM)
 
 #df = pd.DataFrame (A)
 
