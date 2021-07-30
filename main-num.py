a68742656bf2199c59ea4e9c90321b17c2be0870
abhijeet-b <56281551+abhijeet-b@users.noreply.github.com>
2021-07-29 23:24:22 -0500

    Add files via upload

--- main-num.py
+++ main-num.py
@@ -3,114 +3,62 @@ import sympy as sp
 import pandas as pd
 import math
 
+def current_sharing(N, sl, b, f, l, r, Ip):
+    d = 2/((2*math.pi*f)*(4*math.pi*pow(10,-7))*(1.68*math.pow(10,-8)))
+    if (N == 0): quit()
+    A = np.zeros((int(3*N),int(3*N)))
+    serieslist = sl
+    S = len(serieslist)
+    P = N - S
 
-# A = 3N * 3N matrix
-# N = total layers
-# serieslist = list of values in series
-# S = series layers
-# P = parallel layers
-# x = iteration variable
-#b,d,f,l,r=sp.symbols("b,d,f,l,r")
+    parallelList = list(range(1,N+1))
+    for i in serieslist:
+        if i in parallelList:
+            parallelList.remove(i)
+    j = 0
+    for x in serieslist:
+        A[j,x-1] = 1
+        j=j+1
+        if j > S:
+            break
 
-#create array of R symbolic values?
-x=1
-N = int(input('Enter the amount of TOTAL LAYERS your transformer has: '))
-if(N==0):
-	quit()
-A=np.zeros((int(3*N),int(3*N)))
-serieslist = []
+    for x in range(1,N+1):
+        z = N
+        A[j,x-1] = -1
+        A[j,z+2*(x-1)]=b
+        A[j,z+2*(x-1)+1]=b
+        j=j+1
 
+    A[j,N] = 1
+    j = j+1
 
-while 1:
-	x = int(input('Enter, one by one, a series of layer #\'s that are in series: (sentinel is 0)'))
-	if(x==0): break
-	if x in serieslist:
-		print("Already entered this layer #, please enter another layer #")
-	elif((x<0) or (x>N)):
-		print("Out of bounds with range, layer not counted; please enter another value")
-	else:
-		serieslist.append(x)
-"""
-B=np.zeros((int(3*N),int(1)))
-"""
+    for x in range(0,N-1):
+        A[j,N+2*x+1]=1
+        A[j,N+2*x+2]=1
+        j=j+1
+    A[j,3*N-1]=1
+    j=j+1
 
-b = float(input('Layer width in cm:'))/100
-f = float(input('Operating frequency in MHz:'))*1000000
-d = 2/((2*math.pi*f)*(4*math.pi*math.pow(10,-7))*(1.68*math.pow(10,-8)))
-l = float(input('Turn length in cm:'))/100
-r = float(input('Layer spacing in cm:'))/100
+    for x in range(0,P-1):
+        r_range = parallelList[x+1] - parallelList[x]
 
-#print(serieslist)
-serieslist.sort()
+        for k in range(0,parallelList[x+1]-1):
+            if (k+1) > parallelList[x]:
+                r_count = r_range - k - 1 + parallelList[x]
+            else:
+                r_count = r_range
+            A[j,k] = r_count*r*l/b
+        A[j,N+2*parallelList[x]-1]=d/2
+        A[j,N+2*parallelList[x+1]-2]=-d/2
+        j=j+1
+    
+    z=0
+    C=np.zeros((int(3*N),1))
+    for x in parallelList:
+        C[z,0] = Ip
+        z+=1
 
-#b = float(input('Layer width in cm:'))/100;
-#f = float(input('Operating frequency in MHz:'))/1000000
-#d = 2/((2*math.pi*f)*(4*math.pi*math.pow(10,-7))*(1.68*math.pow(10,-8)));
+    M = np.linalg.solve(np.matrix(A),np.matrix(C))
+    return M
+print(current_sharing(8,[2,3,4,7],80,23,48,89,90))
 
-parallelList = list(range(1,N+1))
-for i in serieslist:
-	if i in parallelList:
-		parallelList.remove(i)
-
-#print(parallelList)
-S = len(serieslist)
-P = N - S
-# series identities == all equal to I_p
-j = 0
-for x in serieslist:
-	A[j,x-1] = 1
-	j=j+1
-	if j > S:
-		break
-
-
-for x in range(1,N+1):
-	 z = N
-	 A[j,x-1] = -1
-	 A[j,z+2*(x-1)]=b # A[j][z+2*x-2]
-	 A[j,z+2*(x-1)+1]=b #A[j][z+2*x-1]
-	 j=j+1
-
-
-A[j,N] = 1
-j=j+1
-
-for x in range(0,N-1):
-	A[j,N+2*x+1]=1
-	A[j,N+2*x+2]=1
-	j=j+1
-A[j,3*N-1]=1
-j=j+1
-
-for x in range(0,P-1):
-	r_range = parallelList[x+1] - parallelList[x]
-
-	for k in range(0,parallelList[x+1]-1):
-		if (k+1) > parallelList[x]:
-			r_count = r_range - k -1 + parallelList[x]
-		else:
-			r_count = r_range
-		A[j,k] = r_count*r*l/b
-
-	A[j,N + 2*parallelList[x] - 1] = d/2
-	A[j,N + 2*parallelList[x+1] - 2] = -d/2
-	j=j+1
-
-Ip = float(input("Enter the parallel current, in A: "))
-z=0
-C = np.zeros((int(3*N),1))
-for x in parallelList:
-    C[z,0] = Ip
-    z+=1
-
-#print(A)
-#print(C)
-M = np.linalg.solve(np.matrix(A),np.matrix(C))
-print(M)
-df = pd.DataFrame (A)
-
-# save to xlsx file
-
-filepath = 'table.csv'
-
-df.to_csv(filepath, index=False)
